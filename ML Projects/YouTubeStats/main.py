d38514daa328136fe18c325446edb80d1d0d2776
Cielo Marte <ciemarte@wfscorp.com>
2023-07-25 15:47:49 -0400

    new bug fix

--- ML Projects/YouTubeStats/main.py
+++ ML Projects/YouTubeStats/main.py
@@ -1,5 +1,12 @@
 import sys
 import os
+import argparse
+
+#create parser -> add arguments -> parse arguments --> add datat path to file path
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('data_path', type=str, help='The path to the data file')
+args = parser.parse_args()
+
 
 # Get the absolute path to the directory containing 'main.py'
 current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -14,7 +21,8 @@ from text_classification_model import TextClassifier
 from train_model import train_and_evaluate
 
 if __name__ == "__main__":
-    data_path = "/Users/CieMarte/Documents/ML Projects/YouTubeStats/_Top50_viewed_video_from_each_channels.csv"  
+    data_path = args.data_path
+  
     df = load_and_preprocess_data(data_path)
 
     model = TextClassifier(num_classes=2, embedding_dim=100, vocab_size=10000, hidden_dim=128)
--- ML Projects/YouTubeStats/utils.py
+++ ML Projects/YouTubeStats/utils.py
@@ -1,13 +1,24 @@
 import torch
+import gensim 
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from nltk.tokenize import word_tokenize
+from gensim.models import Word2Vec
 
 def tokenize_text(text):
     # Implement the tokenization logic here using nltk or any other library
     pass
 
 def prepare_text_data(df):
-    # Tokenize the text in the DataFrame and convert it to numerical form using word embeddings
-    pass
+    # Tokenize the text in the DataFrame
+    df['tokens'] = df['title'].apply(lambda x: word_tokenize(x.lower()))
+
+    # Train a Word2Vec model on the tokens to get word embeddings
+    model = Word2Vec(df['tokens'], min_count=1)
+
+    # Convert tokens to embeddings
+    df['embeddings'] = df['tokens'].apply(lambda x: [model.wv[word] for word in x])
+
+    return df
 
 def evaluate_model(model, dataloader):
     model.eval()
